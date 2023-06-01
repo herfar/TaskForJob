08aba03e6db0157eaaa4e1e2e24ba1a7215a2718
herfar <94548763+herfar@users.noreply.github.com>
2023-06-01 17:45:23 +0300

    Update TaskForWork.java

--- TestForOffer/src/main/java/TaskForWork.java
+++ TestForOffer/src/main/java/TaskForWork.java
@@ -3,41 +3,20 @@ import java.util.*;
 public class TaskForWork {
     public static void main(String[] args) {
 
-        int[] arr = new int[]{1, 3, 4, 5, 1, 5, 4};
-
-        System.out.println(new TaskForWork().calculate(arr));
-
+    System.out.print(countIterations(new int [] {1, 3, 4, 5, 1, 5, 4}));
+        
     }
-    public HashMap<Integer, Integer> calculate(int[] arr) {
-        //Переменная счетчик повторений
-        int count = 1;
-
-        //Инициализация
-        LinkedHashSet<Integer> uniqueSet = new LinkedHashSet<>();
-        ArrayList<Integer> list = new ArrayList<>();
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        //Работа метода
-        for (int j : arr) {
-            if (!uniqueSet.add(j)) list.add(j);                                             // Заполнение коллекции, повторяющимися элементами.
-        }
-        if (list.size() > 0) {                                                              // Проверка на содержание повторябщихся элементов в исходном массиве.
-            for (int i = 0; i < uniqueSet.size(); i++) {                                    // Внешний цикл, содержащий элементы без повторений (будущий ключ HashMap).
-                for (int j = 0; j < list.size(); j++) {                                     // Внутренний цикл, содержащий элементы списка с повторениями (будущее значение HashMap).
-                    if (Objects.equals(uniqueSet.stream().toList().get(i), list.get(j))) {  // Сравниваем элементы двух коллекций для колучения количества повторений.
-                        count++;
-                    }
-                    if (j == list.size() - 1) {                                            // Условие на проверку окончания коллекции с ключами HashMap.
-                        map.put(uniqueSet.stream().toList().get(i), count);                // Заполнение HashMap полученными ключами и значеними.
-                        count = 1;                                                         // Обновление счетчика после добавления одного элемента.
-                    }
-                }
-            }
-        } else {                                                                           // Условие для массива без повторений.
-            for (int i = 0; i < uniqueSet.size(); i++) {                                   // Цикл для обхода всех элементов исходного массива/списка.
-                map.put(uniqueSet.stream().toList().get(i), 1);                            // Заполнение HashMap.
+    public static HashMap<Integer, Integer> countIterations(int[] nums) {
+        
+        HashMap<Integer, Integer> mapping = new HashMap<>();
+        
+        for (int num : nums) {
+            if (mapping.containsKey(num)) {
+                mapping.put(num, mapping.get(num) + 1);
+            } else {
+                mapping.put(num, 1);
             }
         }
-        return map;
+        return mapping;
     }
 }
